131445d51f6be59c0ad3224f9816f52c76b5a572
LeFroule <1mpulse1337228@mail.ru>
2023-10-26 21:23:12 +0700

    Made user authorization

--- coolsite/cars/views.py
+++ coolsite/cars/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import logout, login
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
-from .forms import AddPostForm, RegisterUserForm
+from .forms import AddPostForm, RegisterUserForm, LoginUserForm
 from .models import *
 from django.http import HttpResponse
 from .utils import *
@@ -42,11 +44,9 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
 
 
 def contact(request):
-    return HttpResponse('Обратная связь')
-
-
-def login(request):
-    return HttpResponse('Авторизация')
+    data = {'title': 'Обратная связь',
+            'menu': menu}
+    return render(request, 'cars/contact.html', data)
 
 
 class ShowPost(DataMixin, DetailView):
@@ -86,3 +86,23 @@ class RegisterUser(DataMixin, CreateView):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title='Регистрация')
         return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'cars/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
